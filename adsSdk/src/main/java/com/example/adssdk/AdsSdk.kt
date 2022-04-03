e4dd1f07d28ff67e5ed9cf462b17d5a6bf324c91
Vihaan <vihaan@sharechat.co>
2022-04-03 06:35:16 +0530

    adsdk added

---
+++ adsSdk/src/main/java/com/example/adssdk/AdsSdk.kt
@@ -0,0 +1,14 @@
+package com.example.adssdk;
+
+import android.util.Log
+
+object AdsSdk {
+
+    private const val TAG = "AdSdk"
+
+
+    fun init() {
+        Log.d(TAG, "ads sdk init")
+    }
+
+}
